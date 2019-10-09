74e72bf3838ed3f0c1ff59f969bbdcb7f14f3398
cori-peck <cori.peck@ymail.com>
2019-10-09 16:31:10 -0500

    wednesdays lecture and code

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,26 +1,59 @@
-class Stack():
+class Queue():
     def __init__(self):
-        self.stack = []
-    def push(self, value):
-        self.stack.append(value)
-    def pop(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
         if self.size() > 0:
-            return self.stack.pop()
+            return self.queue.pop(0)
         else:
             return None
     def size(self):
-        return len(self.stack)
+        return len(self.queue)
+
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+
+    def add_edges(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist")
 
 
 def earliest_ancestor(ancestors, starting_node, visited=None):
-    if visited is None:
-        visited = set()
-        visited.add(starting_node)
-        #print(starting_node)
-        for next_node in ancestors.vertices[starting_node]:
-            if next_node not in visited:
-                ancestors.dft_recursive(next_node, visited)
-        
+    #build our graph
+    graph = Graph()
 
-ancestors = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
-print(earliest_ancestor(ancestors, 5))
+    for pair in ancestors:
+        graph.add_vertex(pair[0])
+        graph.add_vertex(pair[1])
+
+        #build edges in reverse
+        graph.add_edges(pair[1], pair[0])
+
+    qq = Queue()
+    qq.enqueue([starting_node])
+
+    max_path_length = 1
+    earliest_ancestor = -1
+
+    while qq.size() > 0:
+        path = qq.dequeue()
+        vertex = path[-1]
+
+        if(len(path) >= max_path_length and vertex < earliest_ancestor) or (len(path) > max_path_length):
+            earliest_ancestor = vertex
+            max_path_length = len(path)
+
+        for neighbor in graph.vertices[vertex]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            qq.enqueue(path_copy)
+
+    return earliest_ancestor
